54faf281c98d0381efd9da108d8d5b6b08c12764
beneditorodrigo <rodrigodacunha7@gmail.com>
2022-11-19 14:54:35 -0300

    implementando TestContainers

--- src/test/java/com/beneditorodrigo/parking/service/ParkingCheckOutTest.java
+++ src/test/java/com/beneditorodrigo/parking/service/ParkingCheckOutTest.java
@@ -1,10 +1,11 @@
 package com.beneditorodrigo.parking.service;
 
+import com.beneditorodrigo.parking.controller.AbstractContainerBase;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ParkingCheckOutTest {
+class ParkingCheckOutTest extends AbstractContainerBase {
 
     @Test
     void getBill() {
